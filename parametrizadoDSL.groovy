8fee4fe550863e6266f32d606672e43893d3e8c6
Verónica Tapia <verot500@yahoo.es>
2021-09-05 23:08:43 -0400

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,4 +1,4 @@
-job('ejemplo-job-DSL') {
+job('ejemplo-Nuevo-job-DSL') {
   description('Job DSL de ejemplo para el curso de Jenkins')
   scm {
     git('https://github.com/verot500/jenkins.job.parametrizado.git', 'main') { node ->
